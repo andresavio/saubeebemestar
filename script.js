79d7065d928649d425558a8289e049721c53ec54
andresavio <andresavio.heinzen0@gmail.com>
2025-04-09 15:07:48 -0300

    lol

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Exemplo de funcionalidade JavaScript (ajustar para suas necessidades)
+document.addEventListener("DOMContentLoaded", () => {
+    const articles = document.querySelectorAll('.article a');
+    
+    articles.forEach(article => {
+        article.addEventListener('click', (e) => {
+            alert("Você está prestes a ler um artigo sobre saúde!");
+        });
+    });
+});
 
